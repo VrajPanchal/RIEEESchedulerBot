2a813e2af3bf9284e900d39f81aa5200142aed9c
vraj panchal <51272809+VrajPanchal@users.noreply.github.com>
2022-03-27 06:37:22 +0000

    Correct key

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 
 import discord
 import os
-token = 'OTU3Mzg5MDY3OTI0OTM0NjY2.Yj-EDQ.kV3VcBA66rQPKqQXNwjS-MCrFio'
+
+my_secret = os.environ['token']
 client = discord.Client()
 @client.event
 async def on_ready():
-	print("{0.user} is online!".format.client)
+	print('{0.user}'.format(client) +' is online!')
 @client.event
 async def on_message(message):
 	if message.author == client.user:
 		return
 	elif message.content.startswith('!setMessage'):
 		await message.channel.send('Im alive!')
-client.run(token)
+client.run(my_secret)
